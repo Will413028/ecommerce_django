85028344ce1beb1a6e0ca0be10008fdbd8e18428
william1993314 <william1993314@gmail.com>
2022-01-18 15:09:59 +0800

    Add feature order complete page

--- orders/views.py
+++ orders/views.py
@@ -7,6 +7,7 @@ from store.models import Product
 import json
 from django.core.mail import EmailMessage
 from django.template.loader import render_to_string
+from django.http import HttpResponse, JsonResponse
 
 
 def payments(request):
@@ -62,9 +63,13 @@ def payments(request):
     to_email = request.user.email
     send_email = EmailMessage(mail_subject, message, to=[to_email])
     send_email.send()
-    # Send order number and transaction id back to sendData method via
 
-    return render(request, 'orders/payments.html')
+    # Send order number and transaction id back to sendData method via
+    data = {
+        'order_number': order.order_number,
+        'transID': payment.payment_id,
+    }
+    return JsonResponse(data)
 
 
 def place_order(request, total=0, quantity=0, ):
@@ -127,3 +132,30 @@ def place_order(request, total=0, quantity=0, ):
             return render(request, 'orders/payments.html', context)
         else:
             return redirect('checkout')
+
+
+def order_complete(request):
+    order_number = request.GET.get('order_number')
+    transID = request.GET.get('payment_id')
+
+    try:
+        order = Order.objects.get(order_number=order_number, is_ordered=True)
+        ordered_products = OrderProduct.objects.filter(order_id=order.id)
+
+        subtotal = 0
+        for i in ordered_products:
+            subtotal += i.product_price * i.quantity
+
+        payment = Payment.objects.get(payment_id=transID)
+
+        context = {
+            'order': order,
+            'ordered_products': ordered_products,
+            'order_number': order.order_number,
+            'transID': payment.payment_id,
+            'payment': payment,
+            'subtotal': subtotal,
+        }
+        return render(request, 'orders/order_complete.html', context)
+    except (Payment.DoesNotExist, Order.DoesNotExist):
+        return redirect('home')
