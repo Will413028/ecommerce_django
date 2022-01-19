88dec238f538299ece17edc39d0ee4adbdf578a4
william1993314 <william1993314@gmail.com>
2022-01-19 14:27:33 +0800

    Add my_orders.html

--- accounts/urls.py
+++ accounts/urls.py
@@ -11,4 +11,5 @@ urlpatterns = [
     path('forgetPassword/', views.forgetPassword, name='forgetPassword'),
     path('resetpassword_validate/<uidb64>/<token>/', views.resetpassword_validate, name='resetpassword_validate'),
     path('resetPassword/', views.resetPassword, name='resetPassword'),
+    path('my_orders/', views.my_orders, name='my_orders'),
 ]
