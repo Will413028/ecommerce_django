aabd279fdfc98821b43313d3e1506e57b1c8f8da
william1993314 <william1993314@gmail.com>
2022-01-21 13:06:54 +0800

    Modify the setting for AWS

--- Ecommerce_django/settings.py
+++ Ecommerce_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = ['ecommerce-django-env.eba-u6ifczny.us-east-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['127.0.0.1', '*', ]
 
 # Application definition
 
