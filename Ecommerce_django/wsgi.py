2fbbd68101d3c2cfdf5ed53c3fd49ef25fb961dc
Will413028 <will413028@gmail.com>
2023-03-18 11:03:00 +0800

    django4 project init

--- Ecommerce_django/wsgi.py
+++ Ecommerce_django/wsgi.py
@@ -1,16 +1,16 @@
 """
-WSGI config for Ecommerce_django project.
+WSGI config for ecommerce_django project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ecommerce_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecommerce_django.settings')
 
 application = get_wsgi_application()
