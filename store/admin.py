3de364ce647b27ee19b4d46d59b9db6eed8ddddf
Will413028 <will413028@gmail.com>
2023-03-19 01:41:28 +0800

    feat: add store page

--- store/admin.py
+++ store/admin.py
@@ -1,17 +1,14 @@
 from django.contrib import admin
-
-# Register your models here.
-
 from .models import Category, Product
+
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
     list_display = ['name']
 
-    prepopulated_fields = {'slug': ('name',)}
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['title', 'category', 'brand', 'price']
-
     prepopulated_fields = {'slug': ('title',)}
+    list_display = ['title', 'category', 'brand', 'price']
