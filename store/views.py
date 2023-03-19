d0454bf4118b514564fe21c5097ca7b7fc8b6fd2
Will413028 <will413028@gmail.com>
2023-03-19 14:01:57 +0800

    feat: show every category in navbar

--- store/views.py
+++ store/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from . models import Category
 
 
 def store(request):
 
     return render(request, 'store/store.html')
+
+
+def categories(request):
+
+    all_categories = Category.objects.all()
+
+    print(all_categories)
+    
+    return {'all_categories': all_categories}
