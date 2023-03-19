010841dba3a6b7503cbdcddb345f6c54fb71a574
Will413028 <will413028@gmail.com>
2023-03-19 14:23:17 +0800

    feat: show products in store page

--- store/views.py
+++ store/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
-from . models import Category
+from . models import Category, Product
 
 
 def store(request):
+    all_products = Product.objects.all()
 
-    return render(request, 'store/store.html')
+    context = {'my_products': all_products}
+
+    return render(request, 'store/store.html', context)
 
 
 def categories(request):
 
     all_categories = Category.objects.all()
 
-    print(all_categories)
-    
     return {'all_categories': all_categories}
