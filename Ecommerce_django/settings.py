4c8a670531eb2eead9f6e7880f4944f7e69cf61a
Will413028 <will413028@gmail.com>
2023-03-20 01:10:55 +0800

    feat: use gmail to send verification email

--- Ecommerce_django/settings.py
+++ Ecommerce_django/settings.py
@@ -139,4 +139,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 # Email configuration settings:
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = '587'
+EMAIL_USE_TLS = 'True'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'password'
